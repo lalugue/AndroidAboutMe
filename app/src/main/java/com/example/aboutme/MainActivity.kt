1b9f73d5275b37948107331aec0b805da06da148
lalugue <51893731+lalugue@users.noreply.github.com>
2020-05-21 16:08:16 +0800

    add mechanism for adding nickname

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set listener of button
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View){
+
+        //find nickname_edit component
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        //find nickname_text component
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        //set value of nickname to text in editText
+        nicknameTextView.text = editText.text
+        //hide editText
+        editText.visibility = View.GONE
+        //hide button
+        view.visibility = View.GONE
+        //make nickname appear
+        nicknameTextView.visibility = View.VISIBLE
+
+    }
+
+
 }
